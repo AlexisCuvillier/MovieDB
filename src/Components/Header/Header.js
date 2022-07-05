198302c7b247b1c3aa1a423b26f1290fc078b2ab
AlexisCuvillier <alexis.cuvillier@protonmail.com>
2022-07-05 19:15:11 +0200

    addHeaderAndSearchBar

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+export default function 
+() {
+  return (
+    <div >
+        <div className='backgroundDiv'>
+        <form action="">
+            <input className='inputSearchBar' type="text" placeholder='Rechercher un film' />
+        </form>
+        <button className='buttonSearch'>Rechercher</button>
+        </div>
+    </div>
+  )
+}
